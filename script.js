371ee30e2ee98a2528c820b637e1a4c393dc1b05
ARSamogin <121831671+ARSamogin@users.noreply.github.com>
2025-09-01 11:54:48 -0300

    Changes
    Changes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const Tela = document.getElementById("tela")
 const Add = document.getElementById("aumentar")
 const Min = document.getElementById('diminuir')
 const Reset = document.getElementById("reset")
+const Mais10 = document.getElementById("mais10")
+const Menos10 = document.getElementById("menos10")
 let contagem = 0
 
 Add.onclick = function(){
@@ -16,4 +18,14 @@ Min.onclick = function(){
 
 Reset.onclick = function(){
     Tela.textContent = 0
+}
+
+Mais10.onclick = function(){
+    contagem += 10
+    Tela.textContent = contagem
+}
+
+Menos10.onclick = function(){
+    contagem -= 10
+    Tela.textContent = contagem
 }
